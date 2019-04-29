fc4ad5742ba3b3140104fb9f00a605127ba6e887
Ivan Donato <donato.ivan@gmail.com>
2019-04-29 16:39:07 +1000

    added index, new, create, show to prouducts controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,22 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+  end
+
+  def show
+  end
+
+  private
+  def product_params
+    params.permit(:name, :description, :price, images: [])
+  end
 end
