db06dd0b4f87a2f9445a05d1c663fa8716e96586
Ivan Donato <donato.ivan@gmail.com>
2019-04-29 22:02:35 +1000

    added attributes to wishlist model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  has_many :product_categories
+  has_many :products, through: :product_categories
+  validates :name, presence: true, length: { minimum: 3, maximum: 25}
+  validates_uniqueness_of :name
 end
