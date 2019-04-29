40d37c16652cbb18ff3d12bee376afea42d49f54
Ivan Donato <donato.ivan@gmail.com>
2019-04-29 22:41:06 +1000

    added new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,11 +5,21 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
+    @categories = Category.all.map{|c| [ c.name, c.id ] }
   end
 
   def create
+    @categories = Category.all.map{|c| [ c.name, c.id ] }
     @product = Product.new(product_params)
+    @product.user = current_user
+
+    category = Category.find(params[:category_id1])
+    category2 = Category.find(params[:category_id2])
+    @product.categories << category
+    @product.categories << category2
     @product.save
+
+    redirect_to product_path(@product)
   end
 
   def show
@@ -17,6 +27,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.permit(:name, :description, :price, images: [])
+    params.permit(:name, :description, :price, images: [], category_ids: [])
   end
 end
