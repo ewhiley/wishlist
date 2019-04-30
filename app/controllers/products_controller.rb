dd81c9d8c8c3432266b0b3d53fd77732559aec37
Ivan Donato <donato.ivan@gmail.com>
2019-04-30 14:41:28 +1000

    added product editing and deleting functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,6 +33,30 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    @categories = Category.all.map{|c| [ c.name, c.id ] }
+
+  end
+
+  def update
+    @product = Product.find(params[:id])
+   
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:alert] = "Product was successfully deleted"
+    
+    redirect_to products_path
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price, images: [], category_ids: [])
