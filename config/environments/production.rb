f19569def8040787424648e1732c7546f1968cc2
Ivan Donato <donato.ivan@gmail.com>
2019-04-30 15:42:18 +1000

    test heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => 'https://agile-hamlet-55236.herokuapp.com/' }
+
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
