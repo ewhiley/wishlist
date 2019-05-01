fd66d2e8e3206049bcfa3cdad87390f7bbe2a76c
Ivan Donato <donato.ivan@gmail.com>
2019-05-01 10:50:50 +1000

    added completed column to wishlists table, wishlist_id to purchases table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_133014) do
+ActiveRecord::Schema.define(version: 2019_05_01_002717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_04_30_133014) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "wishlist_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -98,6 +99,7 @@ ActiveRecord::Schema.define(version: 2019_04_30_133014) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
