05b934e31390a6fa0cff18474ecfdb5c69928b50
Ivan Donato <donato.ivan@gmail.com>
2019-05-03 16:19:20 +1000

    added profile editing funtionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @products = Product.all
     @categories = Category.all
+    @user = current_user
   end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,46 @@
 class ProfilesController < ApplicationController
   def index
     @user = current_user
+    @profile = @user.profile
   end
 
   def create
     @user = current_user
-    @profile = Profile.new(profile_params)
+    @profile = Profile.new(profile_params2)
     @profile.user_id = @user.id
     @profile.save
     # byebug
     redirect_to my_profile_path
   end
 
+  def edit
+    @profile = Profile.find(params[:id])
+    @user = current_user
+  end
+
+  def show
+    @profile = Profile.find(params[:id])
+    @user = current_user
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    @user = current_user
+
+    if @profile.update(profile_params)
+      flash[:notice] = "Profile succesfully updated"
+      redirect_to @user.profile
+    else
+      render 'edit'
+    end
+  end
+
   private
   def profile_params
+    params.require(:profile).permit(:name, :phone_no, :street_number, :street_address, :suburb, :state, :postcode)
+  end
+
+  def profile_params2
     params.permit(:name, :phone_no, :street_number, :street_address, :suburb, :state, :postcode)
   end
 end
